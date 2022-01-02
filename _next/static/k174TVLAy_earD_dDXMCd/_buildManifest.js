84cefb1970ebe60c468d39fc8cc76f2901b4814f
Omkar Kamale <checkomkar@users.noreply.github.com>
2022-01-03 01:54:42 +0530

    Deploy gh-pages

--- _next/static/k174TVLAy_earD_dDXMCd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-acb63080e3c00f6a1bab.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f3ad9a057f69aae06b16.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
